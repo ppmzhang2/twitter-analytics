ff87a6ca72378c05f577d3eca00f154f2482aa05
Zhang <mzhang2@paypal.com>
2020-03-16 22:49:09 +0800

    [new] models module

---
+++ app/models/tables.py
@@ -0,0 +1,37 @@
+from ..models.base import Base
+import sqlalchemy as sa
+
+
+class Tweeter(Base):
+    """table to store Wumao users
+    """
+    __tablename__ = 'tweeter'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    user_id = sa.Column(sa.BigInteger)
+    screen_name = sa.Column(sa.String)
+    name = sa.Column(sa.String)
+    created_at = sa.Column(sa.Date)
+    followers = sa.Column(sa.Integer)
+    following = sa.Column(sa.Integer)
+
+    def __init__(self, user_id, screen_name, name, created_at, followers,
+                 following):
+        self.user_id = user_id
+        self.screen_name = screen_name
+        self.name = name
+        self.created_at = created_at
+        self.followers = followers
+        self.following = following
+
+
+class BaseTweeter(Base):
+    """Tweeter ID list from which to search
+    """
+    __tablename__ = 'base_tweeter'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    user_id = sa.Column(sa.BigInteger)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
