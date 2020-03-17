586afce20baf7a0532cfc2bf3b88989348e67bed
Zhang <mzhang2@paypal.com>
2020-03-17 22:01:37 +0800

    [new] loop wumao from base table

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from app.models.dao import Dao
 from app.tweet import Tweet
 from app.models.tables import Tweeter, BaseTweeter
@@ -39,5 +41,46 @@ def save_init_wumao():
     dao.bulk_save(base_tweeter_wumao)
 
 
+def wumao_from_base_top(dao: Dao, tweet: Tweet):
+    user_id = dao.first_base_tweeter().user_id
+    print("potential wumao:", user_id)
+    tweeters = [u for u in tweet.get_following(user_id=user_id)
+                ] + [u for u in tweet.get_followers(user_id=user_id)]
+    wumaos = [u for u in tweeters if tweet.is_junior_wumao(u)]
+    unique_wumaos = [
+        u for u in wumaos if dao.lookup_tweeter_user_id(u.id) is None
+    ]
+    if not wumaos:
+        print("NOT wumao")
+        dao.delete_tweeter_user_id(user_id)
+        dao.delete_base_tweeter_user_id(user_id)
+    elif not unique_wumaos:
+        print("no additional wumao")
+        dao.delete_base_tweeter_user_id(user_id)
+    else:
+        print("inserting new wumaos:", unique_wumaos)
+        tweeter_wumao = [user_to_tweeter(u) for u in unique_wumaos]
+        base_tweeter_wumao = [user_to_base_tweeter(u) for u in unique_wumaos]
+        dao.bulk_save(tweeter_wumao)
+        dao.bulk_save(base_tweeter_wumao)
+        dao.delete_base_tweeter_user_id(user_id)
+
+
+def wumao_loop():
+    d = Dao(new=False)
+    t = Tweet()
+    while True:
+        try:
+            wumao_from_base_top(d, t)
+        except twitter.error.TwitterError as e:
+            if e.message[0]['code'] == 88:
+                # sleep 6 min if exceeds limit
+                sleep(360)
+            else:
+                # else raise error
+                raise e
+
+
 if __name__ == '__main__':
-    save_init_wumao()
+    # save_init_wumao()
+    wumao_loop()
