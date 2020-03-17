51b066c56dd6a5304ec95f441937eb54e0f008b6
Zhang <mzhang2@paypal.com>
2020-03-17 12:43:25 +0800

    [new] tweet module created

--- app/wumao.py
+++ app/wumao.py
@@ -1,27 +1,49 @@
-from config import Config as cfg
+from config import Config
 import twitter
 
-
-class WumaoApi(object):
-    pass
+__all__ = ['Tweet']
 
 
-if __name__ == '__main__':
-    api = twitter.Api(consumer_key=cfg.CONSUMER_KEY,
-                      consumer_secret=cfg.CONSUMER_SECRET,
-                      access_token_key=cfg.ACCESS_TOKEN,
-                      access_token_secret=cfg.ACCESS_TOKEN_SECRET)
+class SingletonMeta(type):
+    _instance = None
 
-    res = api.GetFollowers(user_id=141627220,
-                           cursor=-1,
-                           count=200,
-                           total_count=400,
-                           skip_status=True,
-                           include_user_entities=False)
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instance
 
-    res2 = api.GetFollowers(user_id=141627220,
-                            cursor=0,
-                            count=200,
-                            total_count=400,
-                            skip_status=True,
-                            include_user_entities=False)
+
+class Tweet(metaclass=SingletonMeta):
+    __slots__ = ['api']
+
+    def __init__(self):
+        self.api = twitter.Api(consumer_key=Config.CONSUMER_KEY,
+                               consumer_secret=Config.CONSUMER_SECRET,
+                               access_token_key=Config.ACCESS_TOKEN,
+                               access_token_secret=Config.ACCESS_TOKEN_SECRET)
+
+    def get_followers(self,
+                      user_id,
+                      cursor=-1,
+                      total_count=1000,
+                      skip_status=True,
+                      include_user_entities=False):
+        self.api.GetFollowers(user_id=user_id,
+                              cursor=cursor,
+                              count=total_count,
+                              total_count=total_count,
+                              skip_status=skip_status,
+                              include_user_entities=include_user_entities)
+
+    def get_following(self,
+                      user_id,
+                      cursor=-1,
+                      total_count=1000,
+                      skip_status=True,
+                      include_user_entities=False):
+        self.api.GetFriends(user_id=user_id,
+                            cursor=cursor,
+                            count=total_count,
+                            total_count=total_count,
+                            skip_status=skip_status,
+                            include_user_entities=include_user_entities)
