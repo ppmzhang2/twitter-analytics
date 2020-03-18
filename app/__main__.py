60b6b840d51cc001fe46a728244d0569be238ac3
Zhang <mzhang2@paypal.com>
2020-03-19 02:32:51 +0800

    [new] module main

---
+++ app/__main__.py
@@ -0,0 +1,53 @@
+import sys
+
+from app.saver import Saver
+
+args = sys.argv[1:]
+
+funcs = {
+    'save_pd': (1, Saver, Saver.init_pd_wumao),
+    'save_hxj': (1, Saver, Saver.init_hxj_wumao),
+    'validate': (0, Saver, Saver.validate_wumao)
+}
+
+
+def request():
+    try:
+        return args[0]
+    except IndexError:
+        return None
+
+
+def arg1():
+    """get the 1sr argument to requested function
+
+    :return:
+    """
+    try:
+        return int(args[1])
+    except (IndexError, ValueError):
+        return -1
+
+
+def main():
+    if request() is None:
+        raise TypeError('expect at least one input')
+
+    n_args, cls, func = funcs.get(request(), (None, None, None))
+
+    if func is None:
+        raise TypeError('input request is not valid, accept only {}'.format(
+            list(funcs.keys())))
+
+    if n_args == 0:
+        instance = cls()
+        func(instance)
+    elif n_args == 1:
+        instance = cls()
+        func(instance, arg1())
+    else:
+        raise TypeError('input error')
+
+
+if __name__ == '__main__':
+    main()
