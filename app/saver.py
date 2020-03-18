cfed4d84d8cce194bf91dbb3e13fdc2afcf11efd
Zhang <mzhang2@paypal.com>
2020-03-18 23:42:31 +0800

    [refactor] saver module

--- app/saver.py
+++ app/saver.py
@@ -1,15 +1,13 @@
 from functools import wraps
 from time import sleep
 
-from app.models.dao import Dao
-from app.tweet import Tweet
-from app.models.tables import Tweeter, BaseTweeter
 import twitter
 
-# "PDChinese", People's Daily Chinese
-TWEET_ENTRY_USER_ID_1 = 1531801543
-# "HuXijin_GT"
-TWEET_ENTRY_USER_ID_2 = 1531801543
+from app.models.dao import Dao
+from app.models.tables import Tweeter, BaseTweeter
+from app.tweet import Tweet
+
+__all__ = ['Saver']
 
 
 def _sleep(fn):
@@ -35,99 +33,109 @@ def _sleep(fn):
     return helper
 
 
-def user_to_tweeter(user: twitter.models.User):
-    """convert a twitter.User instance to a Tweeter ORM object
+class SingletonMeta(type):
+    _instance = None
 
-    :param user: a twitter.User instance
-    :return: a Tweeter object
-    """
-    return Tweeter(user.id, user.screen_name, user.name,
-                   Tweet.parse_date(user.created_at), user.followers_count,
-                   user.friends_count)
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instance
 
 
-def user_to_base_tweeter(user: twitter.User):
-    """convert a twitter.User instance to a BaseTweeter ORM object
+class Saver(metaclass=SingletonMeta):
+    # entry point users with mass of wumaos
+    # 1. "PDChinese", People's Daily Chinese
+    # 2. "HuXijin_GT"
+    ENTRY_USER_ID_1 = 1531801543
+    ENTRY_USER_ID_2 = 1531801543
 
-    :param user: a twitter.User instance
-    :return: a Tweeter object
-    """
-    return BaseTweeter(user.id)
+    __slots__ = ['dao', 'tweet']
 
+    def __init__(self):
+        self.dao = Dao(new=False)
+        self.tweet = Tweet()
 
-@_sleep
-def _save_init_wumao(dao, tweet, cursor, user_id, count):
-    print("start saving wumao from cursor:", cursor)
-    next_cursor, old_cursor, seq = tweet.get_followers(user_id=user_id,
-                                                       cursor=cursor,
-                                                       count=count)
-    print("#seq:", len(seq))
-    wumaos = [u for u in seq if Tweet.is_junior_wumao(u)]
-    new_wumaos = [
-        u for u in wumaos if dao.lookup_tweeter_user_id(u.id) is None
-    ]
-    print("#Wumao:", len(wumaos))
-    print("#New Wumao:", len(new_wumaos))
+    @staticmethod
+    def _user_to_tweeter(user: twitter.models.User):
+        """convert a twitter.User instance to a Tweeter ORM object
 
-    if new_wumaos:
-        tweeter_wumao = [user_to_tweeter(u) for u in new_wumaos]
-        base_tweeter_wumao = [user_to_base_tweeter(u) for u in new_wumaos]
-        dao.bulk_save(tweeter_wumao)
-        dao.bulk_save(base_tweeter_wumao)
+        :param user: a twitter.User instance
+        :return: a Tweeter object
+        """
+        return Tweeter(user.id, user.screen_name, user.name,
+                       Tweet.parse_date(user.created_at), user.followers_count,
+                       user.friends_count)
 
-    if next_cursor == 0:
-        print("wumao saving completed")
-        return
-    else:
-        return _save_init_wumao(dao, tweet, next_cursor, user_id, count)
+    @staticmethod
+    def _user_to_base_tweeter(user: twitter.models.User):
+        """convert a twitter.User instance to a BaseTweeter ORM object
 
+        :param user: a twitter.User instance
+        :return: a Tweeter object
+        """
+        return BaseTweeter(user.id)
 
-def save_init_wumao(init_cursor, user_id, n):
-    dao = Dao(new=False)
-    tweet = Tweet()
-    return _save_init_wumao(dao, tweet, init_cursor, user_id, n)
+    @_sleep
+    def init_wumao(self, user_id, cursor=-1, count=200):
+        """save potential wumao users by searching a user's followers
 
+        :param user_id: twitter user whose followers will be searched
+        :param cursor: cursor of tweet.get_followers, default -1
+        :param count: count of tweet.get_followers, default 200
+        :return:
+        """
+        print("start saving wumao from cursor:", cursor)
+        next_cursor, old_cursor, seq = self.tweet.get_followers(
+            user_id=user_id, cursor=cursor, count=count)
+        print("#seq:", len(seq))
+        wumaos = [u for u in seq if Tweet.is_junior_wumao(u)]
+        new_wumaos = [
+            u for u in wumaos if self.dao.lookup_tweeter_user_id(u.id) is None
+        ]
+        print("#Wumao:", len(wumaos))
+        print("#New Wumao:", len(new_wumaos))
 
-def wumao_from_base_top(dao: Dao, tweet: Tweet):
-    user_id = dao.first_base_tweeter().user_id
-    print("potential wumao:", user_id)
-    tweeters = [u for u in tweet.get_following(user_id=user_id)
-                ] + [u for u in tweet.get_followers(user_id=user_id)]
-    wumaos = [u for u in tweeters if tweet.is_junior_wumao(u)]
-    unique_wumaos = [
-        u for u in wumaos if dao.lookup_tweeter_user_id(u.id) is None
-    ]
-    if not wumaos:
-        print("NOT wumao")
-        dao.delete_tweeter_user_id(user_id)
-        dao.delete_base_tweeter_user_id(user_id)
-    elif not unique_wumaos:
-        print("no additional wumao")
-        dao.delete_base_tweeter_user_id(user_id)
-    else:
-        print("inserting new wumaos:", unique_wumaos)
-        tweeter_wumao = [user_to_tweeter(u) for u in unique_wumaos]
-        base_tweeter_wumao = [user_to_base_tweeter(u) for u in unique_wumaos]
-        dao.bulk_save(tweeter_wumao)
-        dao.bulk_save(base_tweeter_wumao)
-        dao.delete_base_tweeter_user_id(user_id)
+        if new_wumaos:
+            tweeter_wumao = [Saver._user_to_tweeter(u) for u in new_wumaos]
+            base_tweeter_wumao = [
+                Saver._user_to_base_tweeter(u) for u in new_wumaos
+            ]
+            self.dao.bulk_save(tweeter_wumao)
+            self.dao.bulk_save(base_tweeter_wumao)
 
+        if next_cursor == 0:
+            print("wumao saving completed")
+            return
+        else:
+            return self.init_wumao(user_id, next_cursor, count)
 
-def wumao_loop():
-    d = Dao(new=False)
-    t = Tweet()
-    while True:
-        try:
-            wumao_from_base_top(d, t)
-        except twitter.error.TwitterError as e:
-            if e.message[0]['code'] == 88:
-                # sleep 6 min if exceeds limit
-                sleep(360)
+    @_sleep
+    def validate_wumao(self):
+        """validate wumao tweeter by checking if its followers or friends are
+        in the initial wumao table
+
+        :return:
+        """
+        zero_user = self.dao.first_base_tweeter()
+        if zero_user is None:
+            print("all wumaos are validated")
+            return
+        else:
+            user_id = zero_user.user_id
+            print("potential wumao:", user_id)
+            related = [
+                u for u in self.tweet.get_following(user_id=user_id)
+            ] + [u for u in self.tweet.get_followers(user_id=user_id)]
+            wumaos = [u for u in related if self.tweet.is_junior_wumao(u)]
+            unique_wumaos = [
+                u for u in wumaos
+                if self.dao.lookup_tweeter_user_id(u.id) is not None
+            ]
+            if not unique_wumaos:
+                print("NO wumao")
+                self.dao.delete_tweeter_user_id(user_id)
+                self.dao.delete_base_tweeter_user_id(user_id)
             else:
-                # else raise error
-                raise e
-
-
-if __name__ == '__main__':
-    # save_init_wumao()
-    wumao_loop()
+                print("confirmed wumao")
+                self.dao.delete_base_tweeter_user_id(user_id)
+            return self.validate_wumao()
